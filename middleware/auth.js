0221f1b41e52a9a39e128f870a0cc717f191d744
BosEriko <bos.eriko@gmail.com>
2019-11-30 17:16:42 +0800

    update the style of auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,18 @@
-const jwt = require('jsonwebtoken');
-const config = require('../config');
+const jwt     = require('jsonwebtoken')
+const config  = require('../config')
 
-module.exports = (req, res, next) =>{
-	const token = req.header('x-auth-token');
+module.exports = (req, res, next) => {
+  const token = req.header('x-auth-token')
 
-	if(!token) return res.status(401).send('Access Denied. No token provided');
+  if(!token) {
+    return res.status(401).send('Access Denied. No token provided.')
+  }
 
- 	try{
- 		let payload = jwt.verify(token, config.secret);
- 		req.user = payload;
- 		next();
- 	}catch(e){
- 		res.status(400).send("Invalid Token");
- 	}
+  try {
+    let payload = jwt.verify(token, config.secret)
+    req.user = payload
+    next()
+  } catch(e) {
+    res.status(400).send("Invalid Token")
+  }
 }
