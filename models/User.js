62535010f65b66473aeba833746e07bbb32d2501
BosEriko <bos.eriko@gmail.com>
2019-11-30 17:20:28 +0800

    update models

--- models/User.js
+++ models/User.js
@@ -1,22 +1,21 @@
-const mongoose = require('mongoose');
-
-const Schema = mongoose.Schema;
+const Mongoose  = require('mongoose')
+const Schema    = Mongoose.Schema
 
 const UserSchema = new Schema({
-	name: String,
-	email:{
-		type: String,
-		required: true,
-		unique: true
-	},
-	isAdmin: {
-		type: Boolean,
-		default: false
-	},
-	password: {
-		type: String,
-		required: true
-	}
+  name: String,
+  email:{
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: Array,
+    default: []
+  }
 })
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = Mongoose.model('User', UserSchema)
