0b110ab56b0856642eb6b4c6550e7a47e9066c22
BosEriko <bos.eriko@gmail.com>
2019-11-30 17:27:15 +0800

    update package.json and server.js

--- server.js
+++ server.js
@@ -1,34 +1,32 @@
-//Express
-const express = require("express");
-const app = express();
+const Express   = require("express")
+const App       = Express()
+const Mongoose  = require("mongoose")
+const Config    = require("./config");
 
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
+App.use(Express.urlencoded({ extended: false }))
+App.use(Express.json())
 
+// Mongoose
+Mongoose.connect("mongodb+srv://admin:dgARDcW2LXxpw7BN@cluster0-hshx4.mongodb.net/test?retryWrites=true&w=majority", {
+  useNewUrlParser:    true, 
+  useUnifiedTopology: true, 
+  useFindAndModify:   false,
+  useCreateIndex:     true
+}).then(() => {
+  console.log("Remote Database Connection Established")
+})
 
-//Mongoose
-const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://admin:dgARDcW2LXxpw7BN@cluster0-hshx4.mongodb.net/test?retryWrites=true&w=majority",{
-	useNewUrlParser:true, 
-	useUnifiedTopology:true, 
-	useFindAndModify:false,
-	useCreateIndex: true
-}).then(()=>{
-	console.log("Remote Database Connection Established");
-});
-
-//config PORT
-const config = require("./config");
-app.listen(config.port, () => {
-	console.log(`Listening on Port ${config.port}`);
-});
+// Config PORT
+App.listen(Config.port, () => {
+	console.log(`Listening on Port localhost:${Config.port}`)
+})
 
 //Router
-const artists = require("./routes/artist_router");
-app.use('/admin', artists);
+const artists = require("./routes/artist_router")
+App.use('/admin', artists)
 
-const users = require("./routes/users_router");
-app.use('/admin', users);
+const users = require("./routes/users_router")
+App.use('/admin', users)
 
-const auth = require("./routes/auth_router");
-app.use('/', auth);
+const auth = require("./routes/auth_router")
+App.use('/', auth)
