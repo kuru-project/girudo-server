9decd354141c810d6c12e08494088aa620933780
BosEriko <bos.eriko@gmail.com>
2019-12-03 05:02:21 +0800

    [AUTO] I am Spartacus

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const App       = Express()
 const Mongoose  = require("mongoose")
 const Config    = require("./config")
 const cors      = require("cors")
+const Stripe    = require('stripe')('sk_test_OuulK18SmBVkvBMb5qji5Gl5006cS2W9S1')
+
 
 App.use(cors())
 
@@ -36,3 +38,22 @@ App.use('/admin', admin)
 App.use('/artist', artist)
 App.use('/book', book)
 App.use('/', auth)
+
+App.post('/charge', (req, res) => {
+  try {
+    Stripe.customers.create({
+      email: "aj.sabandal@gmail.com",
+      description: "some payments",
+      source: "tok_visa"
+    }).then(customers => {
+      Stripe.charges.create({
+        amount: "25000",
+        currency: "php",
+        source: "tok_visa",
+        description: "some charge"
+      })
+    })
+  } catch(e) {
+
+  }
+})
